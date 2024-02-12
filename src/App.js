3c311f5dfa87c163c23624d9d8896a8de63b0e33
aamrahms <90725468+aamrahms@users.noreply.github.com>
2024-02-12 16:58:57 -0500

    Fixed bugs

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import {useState, useEffect} from "react";
 import './App.css';
+import enlargeButton from './App.css';
 import Yes from './Yes';
 
 function App() {
   const [theySaidYes, didTheySayYes]= useState(false);
   const [noCount, setNoCount]= useState(0);
-  // const yesButtonSize= noCount *20 +16;
-  // const yesButtonHeight= yesButtonSize*4;
+  const [takeAwayNo, setTakeAwayNo ]= useState(false);
+  const yesButtonSize= 30;
+  const yesButtonHeight= yesButtonSize*2;
   const sayYes=()=>{
     didTheySayYes(true);
   }
   const sayWhat=()=>{
     setNoCount( noCount+1);
+    if(noCount>= phrases.length-2){
+      setTakeAwayNo(true);
+    }
   }
   const phrases=[
     "",
@@ -24,7 +29,8 @@ function App() {
     "ok, if thats what you really want </3",
     "Last Chance!",
     "I will miss you!!",
-    "I am walking awayyyy..."
+    "I am walking awayyyy...",
+    "there, its gone now... NOW SAY YESSSSSSS"
   ]
   const gifs=[
     "https://i.pinimg.com/564x/fa/1b/8f/fa1b8f5fe6ff8b7c81e191225cc2b99e.jpg",
@@ -37,10 +43,9 @@ function App() {
     "https://i.pinimg.com/originals/d9/5c/5f/d95c5f1fa927d9d0badd6e69bbea6062.gif",
     "https://i.pinimg.com/originals/93/1b/a2/931ba216c319766e5ba9673ca75b8d0e.gif",
     "https://i.pinimg.com/originals/87/6e/1b/876e1ba5fa09fe7b56827e25febc9a4a.gif",
-    "https://i.pinimg.com/originals/3c/ad/fe/3cadfe5a36aff452143b23b334c2b774.gif"
-    // "https://i.pinimg.com/originals/db/9a/31/db9a31068e5698b3affca6fed8b9b670.gif"
+    "https://i.pinimg.com/originals/3c/ad/fe/3cadfe5a36aff452143b23b334c2b774.gif",
+    "https://i.pinimg.com/originals/b7/61/67/b76167d4a1acbf433ab28e4758710dfc.gif"
   ]
-
 
   function getNoButtonText(retType){
     if(retType==="phrases")
@@ -53,25 +58,26 @@ function App() {
       <Yes/>
       :
       <div>
-        {/* <img alt="will-you-be-my-valentine" className="image" src="https://i.pinimg.com/564x/2c/87/83/2c878305a505d28d8d30651cb280e851.jpg"/> */}
         <h1> Will you be my Valentine? </h1>
-        <img className="gif" src={getNoButtonText("gifs")}/>
-        
+        <div>
+        <img src={getNoButtonText("gifs")} />
+        </div>
         <h2> {getNoButtonText("phrases")}</h2>
         <button 
-        className="button-56 yesButton" 
+        className={takeAwayNo? "button-56 yesButton": "button-56 yesButton"}
         name="yes" 
-        // style={{fontSize: yesButtonSize, height: yesButtonHeight}}
+        style={takeAwayNo?  {fontSize: yesButtonSize, height: yesButtonHeight }: {fontSize: "16px", height: "48px"}}
         onClick={sayYes}>
           Yes!
         </button>
-        
-        <button 
+        {takeAwayNo ?
+        <> </>
+        : <button 
         className="button-56 noButton" 
         name="no" 
         onClick={sayWhat}>
           No
-        </button>
+        </button>}
       </div>
       }
 
--- src/Yes.js
+++ src/Yes.js
@@ -7,10 +7,8 @@ export default function Yes() {
             setImageUrl("https://i.pinimg.com/originals/fd/60/15/fd6015dd3f31d0223374f993f66e85d3.gif") 
         },0)
         const url1=setTimeout( ()=>{
-        //   setImageUrl("https://i.pinimg.com/originals/3b/e4/d7/3be4d74d5f8308b2e6edbc605f2efd2e.gif");
-        // setImageUrl("https://i.pinimg.com/originals/04/01/d7/0401d7211d0d78935e6976e1e4628f2f.gif")
-        setImageUrl("https://i.pinimg.com/originals/50/c2/f1/50c2f13c590fdb27c087d6a6736218e0.gif")
-        // div.appendChild(h1);
+       setImageUrl("https://i.pinimg.com/originals/50/c2/f1/50c2f13c590fdb27c087d6a6736218e0.gif")
+        
         },1500)
         
 
